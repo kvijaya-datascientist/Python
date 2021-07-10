147ea8766e95d7152776640b1fb9070bc63f3cfd
vijayalakshmi kaja <61687772+kvijaya-datascientist@users.noreply.github.com>
2021-07-10 23:40:18 +0530

    adding example

---
+++ Basic/ModuleTest1.py
@@ -0,0 +1,6 @@
+import module.Functions as f
+
+area_of_square = f.calculateSquareArea(2)
+print(area_of_square)
+area_of_triangle = f.calculateTriangleArea(2,3)
+print(area_of_triangle)
---
+++ Basic/module/Functions.py
@@ -0,0 +1,9 @@
+"""
+This file has the different functions, which are accessed in other class
+"""
+
+def calculateSquareArea(length):
+    return (length*length)
+
+def calculateTriangleArea(base,height):
+    return 1/2 *(base*height)
