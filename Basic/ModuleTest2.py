12709353575058910e81438b4a73a67ebcf51da9
vijayalakshmi kaja <61687772+kvijaya-datascientist@users.noreply.github.com>
2021-07-11 00:18:54 +0530

    adding example

---
+++ Basic/ModuleTest2.py
@@ -0,0 +1,8 @@
+#required functions file is in location : C:\Users\VijayaKaja\Documents\vijaya_personal_ws\python_ws
+#now,we are accessing the function in the project.
+import sys
+sys.path.append('C:/Users/VijayaKaja/Documents/vijaya_personal_ws/python_ws')
+import Functions1 as f
+
+area = f.calculateSquareArea(2)
+print(area)
