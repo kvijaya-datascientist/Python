a3b9e1e2ff75c29c5a74bbc721acd2d605110e37
vijayalakshmi kaja <61687772+kvijaya-datascientist@users.noreply.github.com>
2021-07-12 23:43:34 +0530

    adding example

--- Basic/read_write_file.py
+++ Basic/read_write_file.py
@@ -1,10 +1,10 @@
 """
-#f = open("C://Vijaya//PyTest.txt","w")  # 'w' - means always overwrite the file content
+#f = open("C://Vijaya//PyTest.txt","w")  # 'w' - opens the file for write only, if file not exist then it will create new one. if already exists then it will over write it
 f=open("C://Vijaya//PyTest.txt","a") # 'a' - means we append the data to the existing file content in the file
 f.write("This is sample file created from python prog and appending the data using 'a'-append mode")
 f.close() """
 
-f= open("C:\\Vijaya\\PyTest1.txt","r") # 'r' -used to read the file
+f= open("C:\\Vijaya\\PyTest1.txt","r") # 'r' - reads a file, if file not exist then it will throw error
 f_out = open("C:\\Vijaya\\PyTest2.txt","w")
 for line in f:
     tokens = line.split(' ')
