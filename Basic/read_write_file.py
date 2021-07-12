60779cd46a4a18176e7006331361fdc32289bc40
vijayalakshmi kaja <61687772+kvijaya-datascientist@users.noreply.github.com>
2021-07-12 23:50:21 +0530

    adding example

--- Basic/read_write_file.py
+++ Basic/read_write_file.py
@@ -4,8 +4,8 @@ f=open("C://Vijaya//PyTest.txt","a") # 'a' - means we append the data to the exi
 f.write("This is sample file created from python prog and appending the data using 'a'-append mode")
 f.close() """
 
-f= open("C:\\Vijaya\\PyTest1.txt","r") # 'r' - reads a file, if file not exist then it will throw error
-f_out = open("C:\\Vijaya\\PyTest2.txt","w")
+f= open("C:\\Vijaya\\PyTest1.txt","r") # 'r' - reads a file, if file not exist then it will throw error. 'r+' - opens file for both reading & writing
+f_out = open("C:\\Vijaya\\PyTest2.txt","w") # 'w+'- opens file for reading & writing.if file doesn't exist then it will create new one.if exists then it will over write.
 for line in f:
     tokens = line.split(' ')
     print(str(tokens))
