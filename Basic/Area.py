d2d2564bb88e2a84a0be86a77bc6cb555c34ab8b
vijayalakshmi kaja <61687772+kvijaya-datascientist@users.noreply.github.com>
2021-07-13 11:18:42 +0530

    adding example for main function in java

---
+++ Basic/Area.py
@@ -0,0 +1,8 @@
+
+def triangle_area(base,height):
+    return 1/2 * (base * height)
+
+if __name__ == "__main__":
+    print("am in Area: main fun")
+    triangleArea = triangle_area(2,3)
+    print(triangleArea)
---
+++ Basic/module/Caller.py
@@ -0,0 +1,8 @@
+import Basic.Area as a
+def calculate_square_area(length):
+    return (length * length)
+
+squareArea = calculate_square_area(2)
+print(squareArea)
+triangleArea = a.triangle_area(2,4)
+print(triangleArea)
